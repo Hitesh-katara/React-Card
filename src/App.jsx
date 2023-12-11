3485e5845e7841b0d610d0d9c54d05747a4de7ee
Hitesh-katara <hkatara03@gmail.com>
2023-12-12 00:04:41 +0530

    react-card

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Card from './components/card'
+
+
+function App() {
+
+  return (
+    <div className='App' >
+      <Card imgSrc="https://picsum.photos/id/16/300/200" imgAlt="card img 16" title="Card Title img16" description="This is the description of Card.You can add more detailes to this description" link="cardPage" buttonText="Learn More"/>
+      <Card imgSrc="https://picsum.photos/id/17/300/200" imgAlt="card img 17" title="Card Title img17" description="This is the description of Card.You can add more detailes to this description" link="cardPage" buttonText="Learn More"/>
+
+      <Card imgSrc="https://picsum.photos/id/18/300/200" imgAlt="card img 18" title="Card Title img18" description="This is the description of Card.You can add more detailes to this description" link="cardPage" buttonText="Learn More"/>
+
+      <Card imgSrc="https://picsum.photos/id/19/300/200" imgAlt="card img 19" title="Card Title img19" description="This is the description of Card.You can add more detailes to this description" link="cardPage" buttonText="Learn More"/>
+
+      <Card imgSrc="https://picsum.photos/id/27/300/200" imgAlt="card img 27" title="Card Title img27" description="This is the description of Card.You can add more detailes to this description" link="cardPage" buttonText="Learn More"/>
+
+    </div >
+  )
+}
+
+export default App
